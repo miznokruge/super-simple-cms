bf6fe6e70ff0265127df7cdd14388390be04e8f5
Mizno <mizno_kruge>
2017-01-15 03:10:43 +0700

    update index.php

--- index.php
+++ index.php
@@ -4,55 +4,55 @@ $isi=file_get_contents("content/".$menu.".html");
 ?>
 <!doctype html>  
 <head>
-<meta charset="UTF-8">
-<title>Androidian- Home</title>
-<link rel="icon" href="images/favicon.gif" type="image/x-icon"/>
+  <meta charset="UTF-8">
+  <title>Androidian- Home</title>
+  <link rel="icon" href="images/favicon.gif" type="image/x-icon"/>
  <!--[if lt IE 9]>
  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-  <![endif]-->
+ <![endif]-->
 
-<link rel="shortcut icon" href="images/favicon.gif" type="image/x-icon"/> 
-<link rel="stylesheet" type="text/css" href="css/styles.css"/>
+ <link rel="shortcut icon" href="images/favicon.gif" type="image/x-icon"/> 
+ <link rel="stylesheet" type="text/css" href="css/styles.css"/>
 
-<link type="text/css" href="css/fancymoves.css" media="screen" charset="utf-8" rel="stylesheet"  />
+ <link type="text/css" href="css/fancymoves.css" media="screen" charset="utf-8" rel="stylesheet"  />
 
 
-  <!-- Required script -->
-  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js" charset="utf-8"></script>
-  <script type="text/javascript" src="js/slider.js" charset="utf-8"></script>
-<style type="text/css" media="screen">
-	@font-face {
+ <!-- Required script -->
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js" charset="utf-8"></script>
+ <script type="text/javascript" src="js/slider.js" charset="utf-8"></script>
+ <style type="text/css" media="screen">
+   @font-face {
     font-family: 'BebasNeueRegular';
     src: url('BebasNeue-webfont.eot');
     src: url('BebasNeue-webfont.eot?#iefix') format('embedded-opentype'),
-         url('BebasNeue-webfont.woff') format('woff'),
-         url('BebasNeue-webfont.ttf') format('truetype'),
-         url('BebasNeue-webfont.svg#BebasNeueRegular') format('svg');
+    url('BebasNeue-webfont.woff') format('woff'),
+    url('BebasNeue-webfont.ttf') format('truetype'),
+    url('BebasNeue-webfont.svg#BebasNeueRegular') format('svg');
     font-weight: normal;
     font-style: normal;
 
-}
+  }
 
-h1.fontface {font: 72px/80px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 0;}
-h2.fontface {font: 35px/43px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 0;}
-h3.fontface {font: 25px/33px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 2px;}
-h4.fontface {font: 20px/28px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 0;}
+  h1.fontface {font: 72px/80px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 0;}
+  h2.fontface {font: 35px/43px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 0;}
+  h3.fontface {font: 25px/33px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 2px;}
+  h4.fontface {font: 20px/28px 'BebasNeueRegular', Arial, sans-serif;letter-spacing: 0;}
 
-p.style1 {font: 18px/27px 'BebasNeueRegular', Arial, sans-serif;}
-		
-	</style>
+  p.style1 {font: 18px/27px 'BebasNeueRegular', Arial, sans-serif;}
 
- 
-  <script type="text/javascript" src="demo/demo.js"></script>
-  
-  <!-- FancyBox scripts -->
-  <script type="text/javascript" src="fancybox-1.3.4/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
-  <script type="text/javascript" src="fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
-  <link rel="stylesheet" type="text/css" href="fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
- 	
-    
-    	<script type="text/javascript">
-		$(document).ready(function() {
+</style>
+
+
+<script type="text/javascript" src="demo/demo.js"></script>
+
+<!-- FancyBox scripts -->
+<script type="text/javascript" src="fancybox-1.3.4/fancybox/jquery.mousewheel-3.0.4.pack.js"></script>
+<script type="text/javascript" src="fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.pack.js"></script>
+<link rel="stylesheet" type="text/css" href="fancybox-1.3.4/fancybox/jquery.fancybox-1.3.4.css" media="screen" />
+
+
+<script type="text/javascript">
+  $(document).ready(function() {
 			/*
 			*   Examples - images
 			*/
@@ -97,134 +97,68 @@ p.style1 {font: 18px/27px 'BebasNeueRegular', Arial, sans-serif;}
 
 		});
 	</script>
-    
-    
 
 
 
-    </head>
-    <body>
 
-  
-    
-   <!-- end pixels -->
-   
-  
 
-    <!--start header-->
+</head>
+<body>
 
-    <header>
- <div class="container">
+
+
+ <!-- end pixels -->
+
+
+
+ <!--start header-->
+
+ <header>
+   <div class="container">
     <!--start title-->
     <h1 class="fontface" id="title"><span class="green">A</span>ndroidian</h1>
 
-	<!--end title-->
-	<!--start menu-->
-	<nav>
-    <ul>
-    <li><a href="?menu=home">Home</a></li>
-    <li><a href="?menu=about">About</a></li>
-	  <li><a href="?menu=work">Portfolio</a></li>
-    <li><a href="?menu=news">News</a></li>
-    <li><a href="?menu=contact">Contact</a></li>
+    <!--end title-->
+    <!--start menu-->
+    <nav>
+      <ul>
+        <li><a href="?menu=home">Home</a></li>
+        <li><a href="?menu=about">About</a></li>
+        <li><a href="?menu=work">Portfolio</a></li>
+        <li><a href="?menu=news">News</a></li>
+        <li><a href="?menu=contact">Contact</a></li>
 
-    </ul>
+      </ul>
     </nav>
   </div>
 
-	<!--end menu-->
+  <!--end menu-->
 
-	
 
-    <!--end header-->
-	</header>
 
- <div id="container">
-   <!--start intro-->
+  <!--end header-->
+</header>
 
-   <section id="intro">
-   <!-- Slider -->
-	<div id="slider-three">
-
-  					<div>
-						<img src="images/demo-images/img1.png" alt="" />
-                   	  	
-                           		<p>Image Title<br />
-                                	
-                   	  </p>
-  					</div>
-        
-  					<div>
-						<img src="images/demo-images/img2.png" alt="" />
-                   	  	<p>Image Title<br />
-                                	
-                            	</p>
-                           		
-  					</div>
-        
-  					<div>
-						<img src="images/demo-images/img3.png" alt="" />
-                   	  	
-                           		<p>Image Title<br />
-                                	
-                   	  </p>
-  					</div>
-        
-  					<div>
-						<img src="images/demo-images/img4.png" alt="" />
-                   	  	
-                           		<p>Image Title<br />
-                                	
-                   	  </p>
-  					</div>
-        
-  					<div>
-						<img src="images/demo-images/img9.png" alt="" />
-                   	  	
-                           		<p>Image Title<br />
-                                	
-                   	  </p>
-  					</div>
-        
-  					
-	</div>
-	<!-- /Slider -->
-    
- 
-   </section>
-   <!--end intro-->
-   
-   
-   
-   <div class="quote">
-   
-   <section class="welcome">
-   <h2 class="fontface"> <span class="green">Welcome</span>.This is our Website.Enjoy!</h2> 
-   </div>
-   <!--end holder-->
-
-   
-   <!--start holder-->
-
+<div id="container">
+ <div class="quote">
    <div class="content1">
     <section class="subcontentmed">
-       <h2 class="fontface"><?=$menu;?></h2>
-       
-        <div style="background:#FFF; color:#000; padding:10px;">
-            <?=$isi;?>
-        </div>
-
-    </section>
+     <h2 class="fontface"><?=$menu;?></h2>
+     <div style="background:#FFF; color:#000; padding:10px;">
+      <?=$isi;?>
     </div>
-   </div>
-   <!--start footer-->
-   <footer>
-   <div class="container">
+
+  </section>
+</div>
+</div>
+<!--start footer-->
+<footer>
+ <div class="container">
    <br><br>
-  <p> © Simple CMS</p> 
-  </div>
-   
-   </footer>
-   <!--end footer--></div>
+   <p> © Simple BUKAN CMS</p> 
+ </div>
+
+</footer>
+<!--end footer--></div>
 <!-- Free template distributed by http://freehtml5templates.com -->   
-   </body></html>
+</body></html>
